896600f94dc252d806721212bbb2abfba63ef5cd
K A I S E R <153696501+KAISSOR@users.noreply.github.com>
2024-08-30 20:25:06 +0300

    edit site

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ fetch('Data_Home_Page.json')
     document.getElementById('offer_content_1').textContent = data.offer_content_1;
     document.getElementById('offer_2').textContent = data.offer_2;
     document.getElementById('offer_content_2').textContent = data.offer_content_2;
+    document.getElementById('offer_3').textContent = data.offer_3;
+    document.getElementById('offer_content_3').textContent = data.offer_content_3;
     document.getElementById('event_title_1').textContent = data.event_title_1;
     document.getElementById('event_title_2').textContent = data.event_title_2;
     document.getElementById('event_content').textContent = data.event_content;
